2e999a58c3c3c6cdca8bc1e278d8acfda89a864f
Carl Knutson <carl@infochimps.org>
2010-03-30 18:47:53 -0500

    Excluded & counted bad data listings - no URL, no tags, long titles

--- data.gov.uk/ukdata_yaml.rb
+++ data.gov.uk/ukdata_yaml.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
+$: << '../../infochimps-data/scaffolds/importYAML/'
 require 'rubygems'
-require '../ImportYAML/importyaml'
+require 'importyaml'
 require 'ukdata_dm'
 
 # Make a new collection called Data.gov.uk
@@ -24,25 +25,48 @@ datagovuk_source = [{'source'=>{'title'=>'Data.gov.uk','description'=>'Data.gov.
 They are drawing on the expertise and wisdom of Sir Tim Berners-Lee and Professor Nigel Shadbolt to publish government data as RDF - enabling data to be linked together.','main_link'=>'http://data.gov.uk/'}}]
 
 all_datasets = []
-sources = []
+# sources = []
+long_title = 0
+no_url = 0
+no_tags = 0
+uploaded = 0
+
+fix_listings = File.open("uk_datasets_to_fixup.tsv","w")
 
 UkDataset.all.each do |dataset|
-  next unless dataset.url != ""
-  next unless dataset.tags != ""
-  
-  data_yaml = DatasetYAML.new(:title => dataset.title, :main_link => dataset.url, :description => dataset.description, :tags => dataset.tags,
-    :owner => "doncarlo", :protected => "true", :collection => "Data.gov.uk")
-  data_yaml.sources = ["Data.gov.uk", dataset.author]
-  sources += [dataset.author] if !(sources.include?(dataset.author))
+  if dataset.url == ""
+    no_url += 1
+    fix_listings << [dataset.id, "Reason: No URL"].join("\t") + "\n"
+    next
+  end
+  if dataset.tags == ""
+    no_tags += 1
+    fix_listings << [dataset.id, "Reason: No tags"].join("\t") + "\n"
+    next
+  end
+  if dataset.title.length > 100
+    long_title += 1
+    fix_listings << [dataset.id, "Title too long: #{dataset.title}"].join("\t") + "\n"
+    next
+  end
+  data_yaml = DatasetYAML.new(:title => dataset.title, :subtitle => "from Data.gov.uk", :main_link => dataset.url.gsub(/\!/,"%21"), :description => dataset.description, :tags => dataset.tags,
+    :owner => "Infochimps", :protected => "true", :collection => "Data.gov.uk")
+  data_yaml.sources = ["Data.gov.uk"]
+#  sources += [dataset.author] if !(sources.include?(dataset.author))
   data_yaml.description += "\n\n" + dataset.extras if dataset.extras != ""
   all_datasets += data_yaml.to_a
+  dataset.uploaded = Time.now.utc
+  dataset.save
+  uploaded += 1
 end
 
-source_yaml = File.open("data.gov.uk_source.yaml","w")
-source_yaml << datagovuk_source.to_yaml
+fix_listings << "Long title count: #{long_title}\nNo url count: #{no_url}\nNo tags count: #{no_tags}\nTotal to fix: #{long_title + no_url + no_tags}\nTotal uploaded: #{uploaded}\n"
 
-collection_yaml = File.open("data.gov.uk_collection.yaml","w")
-collection_yaml << collection
+#source_yaml = File.open("data.gov.uk_source.yaml","w")
+#source_yaml << datagovuk_source.to_yaml
+
+#collection_yaml = File.open("data.gov.uk_collection.yaml","w")
+#collection_yaml << collection.to_yaml
 
 data_yaml = File.open("data.gov.uk_datasets.yaml","w")
 data_yaml << all_datasets.to_yaml
